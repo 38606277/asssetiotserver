b7a22374417b4305d76e0efabc10e09d2245fb2c
XiongBing <905133827@qq.com>
2020-06-24 17:27:48 +0800

    添加通过物联网标签获取资产列表接口

--- src/main/java/root/asset/controller/AssetController.java
+++ src/main/java/root/asset/controller/AssetController.java
@@ -237,5 +237,33 @@ public class AssetController extends RO {
     }
 
 
+    /**
+     * 获取未关联网关的资产列表 来自标签号
+     * @return
+     */
+    @RequestMapping(value = "/listTagNoBindGateway", produces = "text/plain;charset=UTF-8")
+    public String listTagNoBindGateway(@RequestBody JSONObject pJson) throws UnsupportedEncodingException {
+        int currentPage = Integer.valueOf(pJson.getString("pageNum"));
+        int perPage = Integer.valueOf(pJson.getString("perPage"));
+        if (1 == currentPage || 0 == currentPage) {
+            currentPage = 0;
+        } else {
+            currentPage = (currentPage - 1) * perPage;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("startIndex", currentPage);
+        map.put("perPage", perPage);
+        List<Map<String, Object>> assetList = DbFactory.Open(DbFactory.FORM).selectList("eam_asset_status.listTagNoBindGateway", map);
+        int total = DbFactory.Open(DbFactory.FORM).selectOne("eam_asset_status.countTagNoBindGateway", map);
+        Map<String, Object> map2 = new HashMap<String, Object>();
+        Map<String, Object> map3 = new HashMap<String, Object>();
+        map3.put("list", assetList);
+        map3.put("total", total);
+        map2.put("msg", "查询成功");
+        map2.put("data", map3);
+        map2.put("status", 0);
+        return JSON.toJSONString(map2);
+    }
+
 
 }
