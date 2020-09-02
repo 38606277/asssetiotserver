baf6a183cb07b6378256b6cd025fe273f1875937
XiongBing <905133827@qq.com>
2020-09-02 09:49:52 +0800

    添加图片压缩功能

--- src/main/java/root/asset/controller/UploadImgController.java
+++ src/main/java/root/asset/controller/UploadImgController.java
@@ -2,16 +2,24 @@ package root.asset.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import net.coobird.thumbnailator.ThumbnailParameter;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.name.Rename;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import root.report.util.UUIDUtil;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Max;
+import java.awt.image.BufferedImage;
 import java.io.*;
+import java.lang.reflect.Field;
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 //@RequestMapping(value = "/reportServer/gateway")
@@ -20,8 +28,15 @@ public class UploadImgController {
     @Value("${fileDirPath}")
     private String fileDirPath;
 
+
+    @Value("${thumbnailBoundsSize}")
+    private int thumbnailBoundsSize;//缩略图大小 宽高 等比缩放
+
+    @Value("${thumbnailFileSize}")
+    private int thumbnailFileSize;//缩略图文件大小 单位KB
+
     @RequestMapping(value="/uploadAssetImg",produces = "text/plain;charset=UTF-8")
-    public String uploadAssetImg(@RequestParam("file") MultipartFile file) throws UnsupportedEncodingException {
+    public String uploadAssetImg(@RequestParam("file") MultipartFile file)  {
         JSONObject jsonObject = new JSONObject();
 
         if (file.isEmpty()) {
@@ -30,12 +45,21 @@ public class UploadImgController {
 
             return JSON.toJSONString(jsonObject);
         }
-        String fileName = file.getOriginalFilename();
-        String suffix = fileName.substring(fileName.lastIndexOf("."));
-        String newFileName = System.currentTimeMillis() + "_" + UUIDUtil.getUid()  + suffix;
+        String originalFileName = file.getOriginalFilename();
+        String suffix = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String prefix = System.currentTimeMillis() + "_" + UUIDUtil.getUid();
+        String newFileName =  prefix + suffix;
+
         File dest = new File(fileDirPath + newFileName);
         try {
+
+            //保存文件
+            //使用此方法保存必须要绝对路径且文件夹必须已存在,否则报错
             file.transferTo(dest);
+
+            //生成缩略图
+            buildThumbnailImage(dest);
+
             jsonObject.put("resultCode", "1000");
             jsonObject.put("message", "上传成功");
             jsonObject.put("data", new HashMap<String, String>(){{
@@ -48,6 +72,50 @@ public class UploadImgController {
         }
         return JSON.toJSONString(jsonObject);
     }
+
+    /**
+     * 构建缩略图 先压宽高 再压文件大小
+     * @param sourceFile
+     * @throws IOException
+     */
+   private String buildThumbnailImage(File sourceFile) throws IOException {
+       String sourceFilePath = sourceFile.getPath();
+       String thumbnailFileName = "thumbnail_" + sourceFile.getName();
+       String thumbnailFilePath  = fileDirPath + thumbnailFileName;
+
+       String tempFilePath;//缩放后的文件
+
+       if(0 < thumbnailBoundsSize ) {
+           //缩放压缩
+           BufferedImage sourcesBufferedImage  = ImageIO.read(sourceFile);;
+           float height = sourcesBufferedImage.getHeight();
+           float width = sourcesBufferedImage.getWidth();
+           float bounds = Math.max(height,width);
+           float scale = thumbnailBoundsSize / bounds;
+           Thumbnails.of(sourceFile).scale(scale).toFile(thumbnailFilePath);
+           tempFilePath = thumbnailFilePath;
+       }else{
+           tempFilePath = sourceFilePath;
+       }
+
+
+       if(0 < thumbnailFileSize){
+           File tempFile = new File(tempFilePath);
+           //图片尺寸不变，压缩图片文件大小outputQuality实现,参数1为最高质量
+           long size = tempFile.length();
+           double outputQuality = 1.0d ;//压缩比例值
+           if(size >= thumbnailFileSize * 1024){
+               outputQuality = (thumbnailFileSize * 1024f) / size;
+               //压缩图片并保存
+               Thumbnails.of(tempFile).scale(1).outputQuality(outputQuality).toFile(thumbnailFilePath);
+           }
+       }else{
+           Thumbnails.of(tempFilePath).scale(1).toFile(thumbnailFilePath);
+       }
+
+       return thumbnailFileName;
+    }
+
 
     //文件下载相关代码
     @RequestMapping(value = "/downloadAssetImg", produces = "text/plain;charset=UTF-8")
